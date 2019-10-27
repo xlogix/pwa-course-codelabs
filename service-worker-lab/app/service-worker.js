d0977642c1b2c7766d810638f00ae63af9d36299
Uniyal <abhishekuniyal09@gmail.com>
2019-10-27 11:22:17 +0530

    update service worker lab

---
+++ service-worker-lab/app/service-worker.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', event => {
+  console.log('Service worker installing...');
+  // Add a call to skipWaiting here
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service worker activating...');
+});
+
+self.addEventListener('fetch', event => {
+  console.log('Fetching:', event.request.url);
+});
+
+// I'm a new service worker
